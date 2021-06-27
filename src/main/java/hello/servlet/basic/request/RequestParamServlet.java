f5113aafb63f1658866137ed2c76046c58d88335
apdkeb <apdkeb@naver.com>
2021-06-27 15:47:41 +0900

    post 방식으로 조회하기 위한 파일 추가

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -11,16 +11,14 @@ import java.util.Arrays;
 /**
  * 1. 파라미터 전송 기능
  * http://localhost:8080/request-param?username=hello&age=20
+ *
+ *
  */
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-//        String username = request.getParameter("username");
-//        String age = request.getParameter("age");
-//        System.out.println("username = " + username);
-//        System.out.println("age = " + age);
         System.out.println("[전체 파라미터 조회] - start");
         request.getParameterNames().asIterator()
                 .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
