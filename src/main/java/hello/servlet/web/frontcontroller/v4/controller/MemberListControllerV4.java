8b85efa244f397a7fd1d2c8c16ea6212542feff8
apdkeb <apdkeb@naver.com>
2021-07-13 14:49:48 +0900

    v4 - 단순하고 실용적인 예제 마무리

---
+++ src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -0,0 +1,18 @@
+package hello.servlet.web.frontcontroller.v4.controller;
+
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.Map;
+
+public class MemberListControllerV4 implements ControllerV4 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        model.put("members", memberRepository.findAll());
+
+        return "members";
+    }
+}
